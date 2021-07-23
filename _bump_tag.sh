8833c52fd6eef6463bf81bfdafbd9a906c1db52b
Chris Tessmer <chris.tessmer@onyxpoint.com>
2021-07-23 19:52:22 +0000

    Test bump to 1.0.4

--- _bump_tag.sh
+++ _bump_tag.sh
@@ -1,20 +1,54 @@
 #!/bin/bash
 set -euo pipefail
 RPM_SPEC_FILE="build/simp-mockup-component.spec"
-cur_ver="$(grep '^%global main_version' "$RPM_SPEC_FILE" | head -1 | awk '{print $3}')"
-xyz=($(echo $cur_ver  | sed -e 's/\./ /g'))
-y_mod=0; [[ $((1 + $RANDOM % 100)) == 1 ]] && y_mod=1
+RPM_SPEC_TEMPLATE="build/asset-with-multiple-packages.spec.template"
+TAG_RELEASE_WORKFLOW_SOURCE_FILE="workflows/tag_deploy_github-only.yml"
 
-x="${xyz[0]}"
-y=$(( ${xyz[1]} + $y_mod ))
-z=$(( ${xyz[-1]} + 1 ))
-bumped_ver="$x.$y.$z"
-sub_ver=0.8.0
+validate_files_exist()
+{
+  for i in "$RPM_SPEC_FILE" "$RPM_SPEC_TEMPLATE" "$TAG_RELEASE_WORKFLOW_SOURCE_FILE"; do
+    [ -f "$i" ] || { echo "ERROR: File not found: '$i'"; exit 99; }
+  done
+}
 
-sed -e "s/%%MAIN_VER%%/${bumped_ver}/g" -e "s/%%SUB_VER%%/${sub_ver}/g" build/asset-with-multiple-packages.spec.template > "$RPM_SPEC_FILE"
-git add -u
-git commit -m "Test bump to ${bumped_ver}"
-bundle exec rake pkg:create_tag_changelog | tee tag.txt
-git tag "$bumped_ver" -F tag.txt
-git push origin main
-git push origin "$bumped_ver"
+# bump to next x.y.z version (z=always y=1% chance)
+next_rpm_version()
+{
+  local RPM_SPEC_FILE="$1"
+  local cur_ver="$(grep '^%global main_version' "$RPM_SPEC_FILE" | head -1 | awk '{print $3}')"
+  local xyz=($(echo $cur_ver  | sed -e 's/\./ /g'))
+  local y_mod=0; [[ $((1 + $RANDOM % 100)) == 1 ]] && y_mod=1
+  local x="${xyz[0]}"
+  local y=$(( ${xyz[1]} + $y_mod ))
+  local z=$(( ${xyz[-1]} + 1 ))
+  echo "$x.$y.$z"
+}
+
+write_rpm_spec_file_from_template()
+{
+  local RPM_SPEC_TEMPLATE="$1"
+  local RPM_SPEC_FILE="$2"
+  local bumped_ver="$3"
+  local sub_ver="${4:-0.8.0}"
+  sed -e "s/%%MAIN_VER%%/${bumped_ver}/g" -e "s/%%SUB_VER%%/${sub_ver}/g" "$RPM_SPEC_TEMPLATE" > "$RPM_SPEC_FILE"
+}
+
+git_commit_tag_and_push_to_trigger_workflow()
+{
+  git add -u
+  git commit -m "Test bump to ${bumped_ver}"
+  bundle exec rake pkg:create_tag_changelog | tee tag.txt
+  git tag "$bumped_ver" -F tag.txt
+  git push origin main
+  git push origin "$bumped_ver"
+}
+
+# --------------------------------------
+# main
+# --------------------------------------
+validate_files_exist
+bumped_ver="$(next_rpm_version "$RPM_SPEC_FILE")"
+write_rpm_spec_file_from_template "$RPM_SPEC_TEMPLATE" "$RPM_SPEC_FILE" "$bumped_ver"
+git_commit_tag_and_push_to_trigger_workflow "$bumped_ver"
+# --------------------------------------
+
